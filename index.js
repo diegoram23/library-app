bbfeefdc03d07bf272d6e4c2ab0e5822cc35e917
diegoram23 <diego.pta.oc@gmail.com>
2023-04-27 10:33:30 -0700

    succesfully able to remove book from array on click

--- index.js
+++ index.js
@@ -1,40 +1,60 @@
 const title = document.getElementById('title')
 const author = document.getElementById('author')
+const pages = document.getElementById('pages')
+const read = document.getElementById('read')
 const form = document.getElementById('form')
 const books = document.getElementById('books')
 
 let myLibrary = [
     book1 = {
         title: 'the day we met',
-        author: 'michael simmons'
+        author: 'michael simmons',
+        pages: 230,
+        read: 'yes'
     },
     book2 = {
         title: 'the red cat',
-        author: 'Jefferey Bezos'
+        author: 'Jefferey Bezos',
+        pages: 145,
+        read: 'no'
     }
 ]
 
 function getBooksHtml() {
     let booksHtml = ''
     myLibrary.forEach(book => {
+        console.log(book);
         booksHtml += `
         <article>
-        <h3 id="book-title">${book.title}</h3>
-        <strong id="book-author">${book.author}</strong>
+        <h3 id="book-title">Title: ${book.title}</h3>
+        <strong id="book-author">Author: ${book.author}</strong>
+        <p>Number of pages: ${book.pages}</p>
+        <p>Read? ${book.read}</p>
+        <button id='remove-btn' data-remove='${book.title}'>Remove book</button>
     </article>
     `
+
     })
+  
+
     books.innerHTML = booksHtml
 }
 
+document.addEventListener('click', (e) => {
+    if (e.target.dataset.remove) {
+        removeBook(e.target.dataset.remove)
+    }
+})
 
-function Book(title, author) {
+function Book(title, author, pages, read) {
     this.title = title,
-    this.author = author
+        this.author = author,
+        this.pages = pages,
+        this.read = read
 }
 
 function addBookToLibrary() {
-    myLibrary.push(new Book(title.value, author.value))
+    myLibrary.push(new Book(title.value, author.value, pages.value, read.value))
 }
 
 
@@ -46,6 +66,12 @@ form.addEventListener('submit', (e) => {
 
 })
 
+function removeBook(bookId) {
+myLibrary = myLibrary.filter(book => bookId !== book.title)
+getBooksHtml()
+}
+
+console.log(myLibrary);
 getBooksHtml()
 
 
